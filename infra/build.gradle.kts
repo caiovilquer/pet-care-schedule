7663252134b3f432bfd7b661851e99fdf94d19c7
caiovilquer <caiovcarvalho337@gmail.com>
2025-05-22 21:16:42 -0300

    chore: Add kotlin-reflect dependency and test application config
    Added `kotlin-reflect` to support advanced Kotlin features. Created a `application_properties.yml` file for test configurations, setting up an in-memory H2 database and enabling H2 console for easier testing and debugging.

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
 
     implementation("org.flywaydb:flyway-core")
