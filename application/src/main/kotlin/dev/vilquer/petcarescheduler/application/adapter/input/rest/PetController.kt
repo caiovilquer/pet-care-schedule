1ebfdb301140cf2197cfc1823f1a2202a1bc3e73
caiovilquer <caiovcarvalho337@gmail.com>
2025-07-16 19:32:02 -0300

    refactor: refactoring pet adapters to implement domain ports

--- application/src/main/kotlin/dev/vilquer/petcarescheduler/application/adapter/input/rest/PetController.kt
+++ application/src/main/kotlin/dev/vilquer/petcarescheduler/application/adapter/input/rest/PetController.kt
@@ -1,9 +1,9 @@
 package dev.vilquer.petcarescheduler.application.adapter.input.rest
 
 import dev.vilquer.petcarescheduler.application.mapper.PetDtoMapper
-import dev.vilquer.petcarescheduler.application.service.PetAppService
 import dev.vilquer.petcarescheduler.core.domain.entity.PetId
 import dev.vilquer.petcarescheduler.usecase.command.*
+import dev.vilquer.petcarescheduler.usecase.contract.drivingports.*
 import dev.vilquer.petcarescheduler.usecase.result.*
 import org.springframework.http.*
 import org.springframework.web.bind.annotation.*
@@ -11,31 +11,38 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/v1/pets")
 class PetController(
-    private val petService: PetAppService,
-    private val mapper: PetDtoMapper
+    private val mapper: PetDtoMapper,
+    private val createPet: CreatePetUseCase,
+    private val listPets: ListPetsUseCase,
+    private val updatePet: UpdatePetUseCase,
+    private val deletePet: DeletePetUseCase,
+    private val getPet: GetPetUseCase
 ) {
 
     @PostMapping
     fun create(@RequestBody dto: PetDtoMapper.CreateRequest): ResponseEntity<PetCreatedResult> =
-        mapper.toCreateCommand(dto)
-            .let(petService::createPet)
-            .let { ResponseEntity.status(HttpStatus.CREATED).body(it) }
+        ResponseEntity.status(HttpStatus.CREATED)
+            .body(createPet.execute(mapper.toCreateCommand(dto)))
 
     @GetMapping
     fun list(
         @RequestParam page: Int = 0,
         @RequestParam size: Int = 20
-    ): PetsPageResult = petService.listPets(page, size)
+    ): PetsPageResult = listPets.list(page, size)
 
     @PutMapping("/{id}")
     fun update(
         @PathVariable id: Long,
         @RequestBody dto: PetDtoMapper.UpdateRequest
     ): PetDetailResult =
-        mapper.toUpdateCommand(id, dto).let(petService::updatePet)
+        mapper.toUpdateCommand(id, dto).let(updatePet::execute)
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun delete(@PathVariable id: Long) =
-        petService.deletePet(DeletePetCommand(PetId(id)))
+        deletePet.execute(DeletePetCommand(PetId(id)))
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long) =
+        getPet.get(PetId(id))
 }
