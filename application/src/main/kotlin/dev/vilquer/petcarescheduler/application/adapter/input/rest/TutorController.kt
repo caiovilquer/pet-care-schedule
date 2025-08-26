4d8fe2cb525a50594db77bae8e8a8611a42986ff
caiovilquer <caiovcarvalho337@gmail.com>
2025-08-25 21:48:45 -0300

    fix: deleted get request on tutorcontroller

--- application/src/main/kotlin/dev/vilquer/petcarescheduler/application/adapter/input/rest/TutorController.kt
+++ application/src/main/kotlin/dev/vilquer/petcarescheduler/application/adapter/input/rest/TutorController.kt
@@ -7,7 +7,6 @@ import dev.vilquer.petcarescheduler.application.mapper.TutorDtoMapper
 import dev.vilquer.petcarescheduler.core.domain.entity.TutorId
 import dev.vilquer.petcarescheduler.usecase.command.DeleteTutorCommand
 import dev.vilquer.petcarescheduler.usecase.contract.drivingports.*
-import dev.vilquer.petcarescheduler.usecase.result.*
 import org.springframework.http.*
 import org.springframework.web.bind.annotation.*
 import dev.vilquer.petcarescheduler.usecase.result.TutorDetailResult
@@ -17,17 +16,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 @RestController
 @RequestMapping("/api/v1/tutors")
 class TutorController(
-    private val listTutors: ListTutorsUseCase,
     private val updateTutor: UpdateTutorUseCase,
     private val deleteTutor: DeleteTutorUseCase,
     private val getTutor: GetTutorUseCase,
     private val mapper: TutorDtoMapper
 ) {
-
-    @GetMapping
-    fun list(@RequestParam page: Int = 0,
-             @RequestParam size: Int = 20): TutorsPageResult =
-        listTutors.list(page, size)
 
     @PutMapping("/{id}")
     fun update(
